78907c0f4b79b7716030a64ce9ab49279dc2eba1
Magali ERRAMCH <erramchmagali@gmail.com>
2024-06-25 15:15:34 +0200

    mise en place pages et components

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import Footer from "../src/Components/Footer/footer";
+import Header from "../src/Components/Header/header";
+
+import Home from "../src/Pages/Home/home";
+import Login from "../src/Pages/Login/login";
+import Profile from "../src/Pages/Profile/profile";
+import Error from "../src/Pages/Error/error";
+
+export default function App() {
+  const isConnected = useSelector((state) => state.auth.isConnected);
+
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route
+          path="profile"
+          element={isConnected ? <Profile /> : <Navigate to="/login" />}
+        />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
