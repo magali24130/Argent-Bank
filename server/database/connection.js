cb2696d8808e07b2c208d1626290616fb02b11ba
magali24130 <erramchmagali@gmail.com>
2024-07-13 16:34:28 +0200

    init

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
-
+const databaseUrl = "mongodb+srv://Maga24130:Maga1802*@cluster0.hy84ei3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  
 module.exports = async () => {
   try {
     await mongoose.connect(databaseUrl, { useNewUrlParser: true })
